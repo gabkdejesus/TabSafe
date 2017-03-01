80256f2c3d8502d79b581422673148660c0fd1ae
Gab De Jesus <quillot2112@gmail.com>
2017-03-01 20:56:24 +0800

    Pair Name - UrlList saving
    Added saving functionality. Now saves (name-urllist) pairs. 
    Todo on this branch:
    Display (NU) pair after clicking get
    Clean up code 
    Allow saving of multiple (NU) pairs

--- popup.js
+++ popup.js
@@ -1,53 +1,95 @@
+/**
+ * Set div to display some string
+ *
+ * @ param {string} statusText - Text displayed by div, for updates and links
+ *
+ */
 function setStatus(statusText) {
   document.getElementById("status").textContent = statusText;
 }
 
-function addLink(inp) {
-  var link = inp;
-  if(!link) {
+//accepts stringified tab list
+function saveSession(jsontabs) {
+  //  Var name here so that its latest name 
+  var name = document.getElementById("input_name").value;
+  var urllist = jsontabs;
+  if(!name) {
     setStatus("Please enter a session name!");
+    console.log("No session name");
   }
   else {
-    chrome.storage.local.set({"name": link}, function() {
-      setStatus("Saved " + link + "!");
+    chrome.storage.local.set({"name": name, "urls": urllist}, function() {
+      setStatus("Saved " + name + "!");
+      console.log("Saved " + name);
       document.getElementById("input_name").value = "";
-    })
+    });
   }
 }
 
+function getSession() {
+  chrome.storage.local.get("name", function (value) {
+    console.log("Retrieved " + value["name"] + " with urls ");
+    setStatus("Retrieved " + value["name"] + "!");
+  });
+  chrome.storage.local.get("urls", function (value) {
+    var test = JSON.parse(value["urls"]);  // returns the array
+    test.forEach(function(entry) {
+      console.log(entry);
+    })
+  });
+}
 
-// chrome.storage.onChanged.addListener(function(changes, namespace) {
-//         for (key in changes) {
-//           var storageChange = changes[key];
-//           console.log('Storage key "%s" in namespace "%s" changed. ' +
-//                       'Old value was "%s", new value is "%s".',
-//                       key,
-//                       namespace,
-//                       storageChange.oldValue,
-//                       storageChange.newValue);
-//         }
-//       });
-
+/** 
+ * Gets the selected tabs, returns list of urls 
+ * Used with saveSession() to pair session name and urls
+ *
+ */
+function getSelectedTabs(callback) {
+  // Select all highlighted tabs, if none selected, automatically active tab
+  var queryInfo = { highlighted: true };
+  var tabUrlList = new Array();
+  chrome.tabs.query(queryInfo, function(tabs) {
+    console.log("Detected selected tabs:");
+    // for each tab in tabs selected, log the url
+    for(tab in tabs) {
+      var url = tabs[tab].url;
+      tabUrlList.push(url);
+      console.log(url);
+    }
+    if(callback) {
+      callback(tabUrlList);
+    }
+  });
+}
 
 document.addEventListener("DOMContentLoaded", function() {
-  // Listen for submit from form
-  document.getElementById("form").addEventListener("input", function () {
-    var name = document.getElementById("input_name").value;  // Constantly updates
-    document.getElementById("form").addEventListener("submit", function (event) {
-      event.preventDefault();  // Prevents reload due to submit
-      addLink(name);
-    });
+  // Listen for submit from form, same as button click
+  document.getElementById("form").addEventListener("submit", function (event) {
+    event.preventDefault();  // Prevents reload due to submit
+    getSelectedTabs(call);
   });
   // Listen for button click
   document.getElementById("button").onclick = function () {
-    var name = document.getElementById("input_name").value;
-    addLink(name);
+    saveSession();
   };
   // Listen for get button
   document.getElementById("get").onclick = function () {
-    chrome.storage.local.get("name", function (value) {
-      console.log(value);
-      setStatus("Retrieved " + value["name"] + "!");
-    })
+    getSession();
+  };
+  // Get selected tabs url
+  document.getElementById("tab").onclick = function () {
+    getSelectedTabs(call);
   }
 });
+
+// contains the array in array form
+function call(array) {
+  console.log("Selected tabs pushed into list:");
+  console.log(array);
+  console.log("Parsing to JSON")
+  var jsontabs = JSON.stringify(array);
+  console.log("Done parsing");
+  console.log(jsontabs);
+  console.log("now saving");
+  saveSession(jsontabs);
+}
